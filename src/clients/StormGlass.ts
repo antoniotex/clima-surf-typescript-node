3028a9acb6cbf513233a4f71456a8bec8e6b49b7
Antonio Carlos <antoniotx.dev@gmail.com>
2021-07-17 20:42:13 -0300

    Finaliza teste do metodo fetchPoints

--- src/clients/StormGlass.ts
+++ src/clients/StormGlass.ts
@@ -1,18 +1,84 @@
 import { AxiosStatic } from 'axios';
 
+export interface StormGlassPointSource {
+  [key: string]: number;
+}
+
+export interface StormGlassPoint {
+  readonly time: string;
+  readonly waveHeight: StormGlassPointSource;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
+}
+
+export interface StormGlassForecastResponse {
+  hours: StormGlassPoint[];
+}
+
+export interface ForecastPoint {
+  time: string;
+  waveHeight: number;
+  waveDirection: number;
+  swellDirection: number;
+  swellHeight: number;
+  swellPeriod: number;
+  windDirection: number;
+  windSpeed: number;
+}
+
 export class StormGlass {
-  readonly stormGlassAPIKey = '7eef793a-e713-11eb-9f40-0242ac130002-7eef79c6-e713-11eb-9f40-0242ac130002'
+  readonly stormGlassAPIKey =
+    '7eef793a-e713-11eb-9f40-0242ac130002-7eef79c6-e713-11eb-9f40-0242ac130002';
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
-  readonly stormGlassAPISource = 'noaaa';
+  readonly stormGlassAPISource = 'noaa';
   constructor(protected request: AxiosStatic) {}
 
-  public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    return this.request.get(
+  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    const response = await this.request.get<StormGlassForecastResponse>(
       `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`,
-      { headers: {
-        'Authorization': '7eef793a-e713-11eb-9f40-0242ac130002-7eef79c6-e713-11eb-9f40-0242ac130002'
-      }}
+      {
+        headers: {
+          Authorization:
+            '7eef793a-e713-11eb-9f40-0242ac130002-7eef79c6-e713-11eb-9f40-0242ac130002',
+        },
+      }
+    );
+    return this.normalizedResponse(response.data);
+  }
+
+  private normalizedResponse(
+    points: StormGlassForecastResponse
+  ): ForecastPoint[] {
+    // Nesse caso precisei fazer o bind, pois como é um metodo de uma classe, pertence a uma instancia de StormGlass
+    // Se nao fizer o this, vai dar undefined
+    return points.hours.filter(this.isValidPoint.bind(this)).map((point) => ({
+      time: point.time,
+      swellDirection: point.swellDirection[this.stormGlassAPISource],
+      swellHeight: point.swellHeight[this.stormGlassAPISource],
+      swellPeriod: point.swellPeriod[this.stormGlassAPISource],
+      waveDirection: point.waveDirection[this.stormGlassAPISource],
+      waveHeight: point.waveHeight[this.stormGlassAPISource],
+      windDirection: point.windDirection[this.stormGlassAPISource],
+      windSpeed: point.windSpeed[this.stormGlassAPISource],
+    }));
+  }
+
+  // O Partial faz com que as props virem opcionais ou possam ser undefined
+  private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+    return !!(
+      point.time &&
+      point.waveHeight?.[this.stormGlassAPISource] &&
+      point.waveDirection?.[this.stormGlassAPISource] &&
+      point.swellDirection?.[this.stormGlassAPISource] &&
+      point.swellHeight?.[this.stormGlassAPISource] &&
+      point.swellPeriod?.[this.stormGlassAPISource] &&
+      point.windDirection?.[this.stormGlassAPISource] &&
+      point.windSpeed?.[this.stormGlassAPISource]
     );
   }
 }
