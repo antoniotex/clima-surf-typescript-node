04b28e89b3772a2703607820eff82e3705e1a21b
Antonio Carlos <acm.skt@gmail.com>
2021-07-21 23:09:42 -0300

    Cria baeController e refina erros do model user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,12 +1,14 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
+import { AxiosError } from 'axios';
+import { BaseController } from '.';
 // import AuthService from '@src/services/auth';
 // import { BaseController } from './index';
 // import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
     try {
@@ -14,7 +16,7 @@ export class UsersController {
       const newUser = await user.save();
       res.status(201).send(newUser);
     } catch (error) {
-      // this.sendCreateUpdateErrorResponse(res, error);
+      this.sendCreatedUpdateErrorResponse(res, (error as Error))
     }
   }
 }
